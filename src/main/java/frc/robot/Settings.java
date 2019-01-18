b1f04ce8a74083c5b52a6cc2c25ebb8a4198995c
mayarouth <routhmaya@gmial.com>
2019-01-18 16:24:26 -0600

    Untested. 3 separate solenoids (3 needed)

--- src/main/java/frc/robot/Settings.java
+++ src/main/java/frc/robot/Settings.java
@@ -12,9 +12,14 @@ public class Settings {
   public static final int backLeftPort   = 2;
   public static final int backRightPort  = 3;
   
-  //solenoid
-  public static final int solenoidForwardPort  = 0;
-  public static final int solenoidBackwardPort = 1;
+  //solenoids
+  public static final int solenoidForwardPort1  = 0;
+  public static final int solenoidBackwardPort1 = 1;
+  public static final int solenoidForwardPort2 = 2;
+  public static final int solenoidBackwardPort2 = 3;
+  public static final int solenoidForwardPort3 = 4;
+  public static final int solenoidBackwardPort3 = 5;
+
 
   //controller
   public static final int controllerNumber = 0;
--- src/main/java/frc/robot/TestSolenoid.java
+++ src/main/java/frc/robot/TestSolenoid.java
@@ -6,29 +6,53 @@ package frc.robot;
 
 import edu.wpi.first.wpilibj.DoubleSolenoid;
 import edu.wpi.first.wpilibj.XboxController;
+//never used message
+/* import edu.wpi.first.wpilibj.DoubleSolenoid.Value; */
+import edu.wpi.first.wpilibj.GenericHID.Hand;
 
 public class TestSolenoid {
 
   //Declare variables
-  private DoubleSolenoid testSolenoid;
+  private DoubleSolenoid solenoid1;
+  private DoubleSolenoid solenoid2;
+  private DoubleSolenoid solenoid3;
 
   private XboxController controller;
 
   //contructor
   public TestSolenoid(XboxController controller) {
-    testSolenoid = new DoubleSolenoid(Settings.solenoidForwardPort, Settings.solenoidBackwardPort);
+    solenoid1 = new DoubleSolenoid(Settings.solenoidForwardPort1, Settings.solenoidBackwardPort1);
+    solenoid2 = new DoubleSolenoid(Settings.solenoidForwardPort2, Settings.solenoidBackwardPort2);
+    solenoid3 = new DoubleSolenoid(Settings.solenoidForwardPort3, Settings.solenoidBackwardPort3);
 
     this.controller = controller;
   }
 
   public void update() {
     if(controller.getAButton()) {
-      testSolenoid.set(DoubleSolenoid.Value.kForward);
+      solenoid1.set(DoubleSolenoid.Value.kForward);
     } else if(controller.getBButton()) {
-      testSolenoid.set(DoubleSolenoid.Value.kReverse);
+      solenoid1.set(DoubleSolenoid.Value.kReverse);
     } else {
-      testSolenoid.set(DoubleSolenoid.Value.kOff);
+      solenoid1.set(DoubleSolenoid.Value.kOff);
     }
+
+    if(controller.getBumper(Hand.kLeft)) {
+      solenoid2.set(DoubleSolenoid.Value.kForward);
+    } else if(controller.getXButton()) {
+      solenoid2.set(DoubleSolenoid.Value.kReverse);
+    } else {
+      solenoid2.set(DoubleSolenoid.Value.kOff);
+    }
+    
+    if(controller.getBumper(Hand.kRight)) {
+      solenoid3.set(DoubleSolenoid.Value.kForward);
+    } else if(controller.getYButton()) {
+      solenoid3.set(DoubleSolenoid.Value.kReverse);
+    } else {
+      solenoid3.set(DoubleSolenoid.Value.kOff);
+    }
+    
   }
 
 }
