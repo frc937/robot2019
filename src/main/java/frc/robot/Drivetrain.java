3ae68077052a132459f5da73f730ca2eb569cdc5
mayarouth <routhmaya@gmail.com>
2019-02-08 17:46:24 -0600

    drivetrain changes because drivetrain was broken. last commit/push didn't work so hopefully this will.

--- src/main/java/frc/robot/Drivetrain.java
+++ src/main/java/frc/robot/Drivetrain.java
@@ -22,6 +22,10 @@ public class Drivetrain {
 
   private XboxController controller;
 
+  private double r;
+  private double robotAngle;
+  private double rightX;
+
   //constructor (run whenever the: = new Drivetrain() code is run)
   public Drivetrain(XboxController controller) {
     frontLeft = new Talon(Settings.frontLeftPort);
@@ -29,8 +33,8 @@ public class Drivetrain {
     backLeft = new Talon(Settings.backLeftPort);
     backRight = new Spark(Settings.backRightPort);
 
-    frontLeft.setInverted(true);
-    backLeft.setInverted(true);
+    frontLeft.setInverted(false);
+    backLeft.setInverted(false);
 
     drivetrain = new MecanumDrive(frontLeft, backLeft, frontRight, backRight);
 
@@ -38,12 +42,25 @@ public class Drivetrain {
   }
 
   public void driverControl() {
-    // Use the joystick X axis for lateral movement, Y axis for forward movement, and Z axis for rotation.
-    final double x = controller.getX(Hand.kLeft);
-    final double y = controller.getY(Hand.kLeft);
-    final double z = controller.getX(Hand.kRight);
+    r = Math.hypot(controller.getX(Hand.kLeft), controller.getY(Hand.kLeft));
+    robotAngle = Math.atan2(controller.getY(Hand.kLeft), controller.getX(Hand.kLeft)) - Math.PI / 4;
+    rightX = controller.getX(Hand.kRight);
+    final double v1 = r * Math.cos(robotAngle) + rightX;
+    final double v2 = r * Math.sin(robotAngle) - rightX;
+    final double v3 = r * Math.sin(robotAngle) + rightX;
+    final double v4 = r * Math.cos(robotAngle) - rightX;
 
-    drivetrain.driveCartesian(x, y, z, 0.0);
+    frontLeft.setSpeed(v1);
+    frontRight.setSpeed(v2);
+    backLeft.setSpeed(v3);
+    backRight.setSpeed(v4);
+    
+    // Use the joystick X axis for lateral movement, Y axis for forward movement, and Z axis for rotation.
+    //final double x = controller.getX(Hand.kLeft);
+    //final double y = controller.getY(Hand.kLeft);
+    //final double z = controller.getX(Hand.kRight);
+
+    //drivetrain.driveCartesian(x, y, z, 0.0);
   }
 
 }
