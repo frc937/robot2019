e7b9fca3a8a04301aab8d123ba8dc35d913f0ed9
mayarouth <routhmaya@gmail.com>
2019-09-22 15:36:52 -0500

    Valuable info

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -1,6 +1,25 @@
 /*
- * Main loops and methods for the robot
- */
+* This class contains
+* Main loops and methods for the robot
+*/
+
+/*
+* Some valuable info:
+* 
+* If you need help, this directory is a good place to start
+* https://wpilib.screenstepslive.com/s/4485
+*
+* Here's a whole index of classes and methods in them that you can 
+* import and use as I've done below. 
+* https://first.wpi.edu/FRC/roborio/release/docs/java/index-all.html#
+*
+* Use those links. They will help you.
+*
+* Google is also a valuable tool. Use it to your advantage.
+*
+* Don't shy away from digging through FRC java programming forums
+* to see other people's solutions to problems similar to yours.
+*/
 
 package frc.robot;
 
